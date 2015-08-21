7060cab1f6993076b3202984f92bf4e5e43e6699
Dmitry Teselkin <teselkin.d@gmail.com>
2015-08-21 21:03:40 +0300

    Update

--- summary.py
+++ summary.py
@@ -3,6 +3,9 @@
 import json
 from sh import repoquery
 
+INTERNAL_REPO = ['mos61']
+EXTERNAL_REPO = ['base', 'epel']
+
 
 def check_other_repos(name, repolist=['base', 'epel']):
     queryfmt = '{"name":"%{name}","version":"%{version}",' \
@@ -20,9 +23,9 @@ def check_other_repos(name, repolist=['base', 'epel']):
 
 def compare_versions(a, b):
     try:
-#        if a['epoch'] != b['epoch']:
-#            return 'NO_MATCH'
-#
+        if a['epoch'] != b['epoch']:
+            return 'NO_MATCH'
+
         if a['version'] != b['version']:
             return 'NO_MATCH'
 
@@ -40,29 +43,41 @@ with open('packages.json') as f:
 with open('backrefs.json') as f:
     backrefs = json.load(f)
 
-#print 'Items in packages.json - %s' % len(packages.keys())
-#print 'Items in backrefs.json - %s' % len(backrefs.keys())
+print 'Items in packages.json - %s' % len(packages.keys())
+print 'Items in backrefs.json - %s' % len(backrefs.keys())
 
+fmt_string = '%s; %s; %s:%s-%s; %s; %s; %s; %s; %s\n'
 with open('summary.csv', 'w') as f:
     for name in packages.keys():
-        other_packages = check_other_repos(name)
-        if other_packages:
-            for pkg in other_packages:
-                match = compare_versions(pkg, packages[name])
-                pkg_string = '%s %s-%s:%s-%s' % (pkg['repoid'], pkg['name'],
-                                                 pkg['epoch'], pkg['version'],
-                                                 pkg['release'])
-                f.write('%s; %s; %s; %s; %s; %s\n' % (name,
-                                                    packages[name]['version'],
-                                                    packages[name]['release'],
-                                                    backrefs[name]['count'],
-                                                    match,
-                                                    pkg_string))
-        else:
-            f.write('%s; %s; %s; %s; %s; %s\n' % (name,
-                                                packages[name]['version'],
-                                                packages[name]['release'],
-                                                backrefs[name]['count'],
-                                                'NO_MATCH',
-                                                ''))
+        found_externally = False
+        for repoid in INTERNAL_REPO:
+            pkg_int = packages[name][repoid]
+            for repoid_ext in EXTERNAL_REPO:
+                if repoid_ext not in packages[name]:
+                    continue
+                found_externally = True
+                pkg_ext = packages[name][repoid_ext]
+                match = compare_versions(pkg_int, pkg_ext)
+                pkg_string = '%s-%s:%s-%s' % (pkg_ext['name'],
+                                              pkg_ext['epoch'],
+                                              pkg_ext['version'],
+                                              pkg_ext['release'])
+                f.write(fmt_string % (name,
+                                      backrefs[name]['count'],
+                                      pkg_int['epoch'],
+                                      pkg_int['version'],
+                                      pkg_int['release'],
+                                      pkg_int['vendor'],
+                                      match,
+                                      pkg_ext['repoid'],
+                                      pkg_string,
+                                      pkg_ext['vendor']))
+            if not found_externally:
+                f.write(fmt_string % (name,
+                                      backrefs[name]['count'],
+                                      pkg_int['epoch'],
+                                      pkg_int['version'],
+                                      pkg_int['release'],
+                                      pkg_int['vendor'],
+                                      'NO_MATCH', '', '', ''))
 
